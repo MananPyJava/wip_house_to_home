7ac27550798d457e70bc2b6585182542d8f2cbc8
Manan Gandhi <67302570+MananPyJava@users.noreply.github.com>
2021-03-30 20:30:37 +0530

    Update urls.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('contact', views.contact,name='contact'),
     path(r'^googlea14532fb912s486\.html$', lambda r: HttpResponse("google-site-verification: ./google0c6908d28b29a56a.html", mimetype="text/plain")),
 ]
-handler404='my_project.views.no_url'
-handler500='my_project.views.no_url2'
+handler404='my_project.views.google_verification'
+handler500='my_project.views.google_verification'
